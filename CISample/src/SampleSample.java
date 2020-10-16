39ad63ae3012110fa1050144ed33ffcc8d8cb8a1
viji25590 <viji25590@gmail.com>
2020-10-16 23:57:35 +0530

    Version1

---
+++ CISample/src/SampleSample.java
@@ -0,0 +1,13 @@
+
+public class SampleSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("How u doin");
+		System.out.println("Hell is fill with people like you");
+		System.out.println("Well done");
+		System.out.println("Did i said it louder");
+	}
+
+}
