d4d7f51d5355d3a1025927801966a0bc5162be75
viji25590 <72642130+viji25590@users.noreply.github.com>
2022-02-11 23:23:13 +0530

    Update Dump.java

--- Sample1/src/Dump.java
+++ Sample1/src/Dump.java
@@ -3,7 +3,7 @@ public class Dump {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Delete this projetct");
+
 		System.out.println("this line added");
 	}
 
