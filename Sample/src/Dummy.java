ef40f997f2a3e0bd08a1747557bac7264dc9024f
viji25590 <72642130+viji25590@users.noreply.github.com>
2022-02-12 23:40:58 +0530

    comment line added
    comments

--- Sample/src/Dummy.java
+++ Sample/src/Dummy.java
@@ -4,7 +4,10 @@ public class Dummy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	//added comments for git
 		System.out.println("Delete this");
+	
+	
 	}
 
 }
